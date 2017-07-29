288bafb31e124ed63af7d098aa378daf8777e3f8
debjyotiSRoy <debjyoti.saharoy@gmail.com>
2017-07-30 01:43:40 +0530

    test branch

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		SalesData data = new SalesData();
 		
+		System.out.println("This is a test branch")
+		
 		displayGreeting();
 		data.display();
 	}
